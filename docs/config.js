a612f138204b28bf1bcc93e2ecd40b0f57a5f69f
lgivhan <youngturkay@gmail.com>
2026-01-04 14:36:14 -0500

    make publishable key public

---
+++ docs/config.js
@@ -0,0 +1,5 @@
+const CONFIG = {
+  SUPABASE_URL: 'https://xdphtnnxvrhbhbwpjffh.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_JiXHJTCMjNtqgVVXbnhqng_AGpsqRg9',
+  STOCK_API_KEY: 'YOUR_STOCK_API_KEY_HERE'
+};
