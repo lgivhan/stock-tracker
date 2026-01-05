ba97a963f276f261513665e460366c07aad7d4ec
lgivhan <youngturkay@gmail.com>
2026-01-05 12:56:34 -0500

    add stock delete option, correct column names, add stock api

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   SUPABASE_URL: 'https://xdphtnnxvrhbhbwpjffh.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_JiXHJTCMjNtqgVVXbnhqng_AGpsqRg9',
-  STOCK_API_KEY: 'YOUR_STOCK_API_KEY_HERE'
+  STOCK_API_KEY: '2D8OS1Y5ITM3Q5HS'
 };
